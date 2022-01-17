55d4d120a35b220346f6a31e343a5fe0bd710d33
lijialiang <lijialiang@joyy.com>
2022-01-17 18:31:46 +0800

    build: babel compile

--- rollup.config.js
+++ rollup.config.js
@@ -3,6 +3,9 @@ import livereload from 'rollup-plugin-livereload'
 import { terser } from 'rollup-plugin-terser'
 import banner from 'rollup-plugin-banner'
 import typescript from 'rollup-plugin-typescript2'
+import { babel } from '@rollup/plugin-babel'
+import resolve from '@rollup/plugin-node-resolve'
+import commonjs from '@rollup/plugin-commonjs'
 
 const FORMAT = process.env.FORMAT
 const IS_TEST_ENV = process.env.NODE_ENV === 'test'
@@ -20,16 +23,36 @@ const config = {
     sourcemap: false
   },
   plugins: [
+    resolve({ jsnext: true, preferBuiltins: true, browser: true }),
+    commonjs(),
     typescript({
       tsconfig: IS_TEST_ENV ? 'tsconfig.test.json' : 'tsconfig.json'
+    }),
+    babel({
+      exclude: /node_modules/,
+      babelrc: false,
+      babelHelpers: 'runtime',
+      presets: [
+        '@babel/preset-env'
+      ],
+      plugins: [
+        [
+          '@babel/plugin-transform-runtime',
+          { targets: '> 0.25%, not dead' }
+        ]
+      ]
     }),
     IS_TEST_ENV && serve(TEST_DIR),
     IS_TEST_ENV && livereload({
       watch: TEST_DIR,
       verbose: false
     }),
-    !IS_TEST_ENV && terser(),
-    !IS_TEST_ENV && banner('v<%= pkg.version %>')
+    !IS_TEST_ENV && terser({
+      output: {
+        comments: false
+      }
+    }),
+    !IS_TEST_ENV && banner('https://github.com/lijialiang/gifff v<%= pkg.version %>')
   ]
 }
 
